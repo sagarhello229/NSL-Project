2f82771d46227348d371e4b64210c581b02e6a3f
Riya Sunar <137243584+Riya207@users.noreply.github.com>
2025-06-22 13:11:32 +0545

    chnage on processing

--- server/utils/preprocess.py
+++ server/utils/preprocess.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-# augmentation gareko
+# Augmentation parameters for training
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     rotation_range=10,
@@ -14,17 +14,26 @@ train_datagen = ImageDataGenerator(
     fill_mode='nearest'
 )
 
-# test-time preprocessing
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 def preprocess_image(image, target_size=(96, 96), for_training=False):
+    """
+    Resize, convert color, normalize and optionally augment image.
+    
+    Args:
+        image (numpy.ndarray): Input image (BGR format from OpenCV)
+        target_size (tuple): Desired size (width, height)
+        for_training (bool): Whether to apply augmentation
+
+    Returns:
+        numpy.ndarray: Preprocessed image with shape (1, H, W, 3)
+    """
     image = cv2.resize(image, target_size)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = np.expand_dims(image, axis=0)  
+    image = image.astype("float32") / 255.0
+    image = np.expand_dims(image, axis=0)
 
     if for_training:
-        image = next(train_datagen.flow(image, batch_size=1))
-    else:
-        image = image.astype("float32") / 255.0 
+        image = next(train_datagen.flow(image, batch_size=1, shuffle=False))
 
-    return image  
+    return image
